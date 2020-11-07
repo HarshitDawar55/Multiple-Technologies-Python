bf4709f393579954a1e94ab289e76f3f9bc74874
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-11-07 20:16:07 +0530

    optimized the funciton!

--- linuxbasiccmnd&docker.py
+++ linuxbasiccmnd&docker.py
@@ -7,22 +7,22 @@ def RunCommand(cmd):
 
 
 def SpecificCommands():
-    print("""Functionality supported by this software are listed below:\n
-           Press 1 to check Date\n
-           Press 2 to check Calender\n
-           Press 3 to create a Directory\n
-           Press 4 to create a File\n
-           Press 5 to Enter Data into a file\n
-           Press 6 to move a file\n
-           Press 7 to check the OS Details\n
-           Press 8 to check the IP of the System\n
-           Press 9 to list the contents of a directory\n
-           Press 10 to see the content of a file\n
-           Press 11 to ping to a website\n
-           Press 12 to add a user in the system\n
-           Press 13 to remove a user\n
-           Press 14 to list the mounted partitions\n
-           Press 15 to list all the block partitions\n""")
+    print("""Functionality supported by this software are listed below:
+    Press 1 to check Date
+    Press 2 to check Calender
+    Press 3 to create a Directory
+    Press 4 to create a File
+    Press 5 to Enter Data into a file
+    Press 6 to move a file
+    Press 7 to check the OS Details
+    Press 8 to check the IP of the System
+    Press 9 to list the contents of a directory
+    Press 10 to see the content of a file
+    Press 11 to ping to a website
+    Press 12 to add a user in the system
+    Press 13 to remove a user
+    Press 14 to list the mounted partitions
+    Press 15 to list all the block partitions""")
     ch = int(input())
 
     if ch == 1:
@@ -75,11 +75,9 @@ def SpecificCommands():
     elif ch == 15:
         os.system("lsblk")
 
-
+print(""""//////////////////////////////Important Note: This program is best supported on OS based on CentOS//////////////////////////""")
 while True:
-    print("""//////////////////////////////////////////////Important Note: This program is best supported on OS based 
-    on CentOS//////////////////////////////////////////////\n Press 1: To Run Command Locally!\nPress 2: To run 
-    Command Remotely!\n Press 3 To exit from the Program!\n""")
+    print(" Press 1: To Run Command Locally!\n Press 2: To run Command Remotely!\n Press 3 To exit from the Program!\n")
     choice = int(input())
 
     if choice == 1:
