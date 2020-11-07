bd25d67e51747b0d24401cb8e6e0f71d58481ad8
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-11-07 20:09:51 +0530

    Added the basic functionalities support!

--- linuxbasiccmnd&docker.py
+++ linuxbasiccmnd&docker.py
@@ -7,7 +7,7 @@ def RunCommand(cmd):
 
 
 def SpecificCommands():
-    print("""Functionalities supported by this software are listed below:\n
+    print("""Functionality supported by this software are listed below:\n
            Press 1 to check Date\n
            Press 2 to check Calender\n
            Press 3 to create a Directory\n
@@ -46,15 +46,44 @@ def SpecificCommands():
         os.system("mv {} {}".format(initialPath, FinalPath))
     elif ch == 7:
         print(os.system("cat /etc/os-release"))
+    elif ch == 8:
+        print("Enter 1 if you are Virtual Machine else Press 2\n")
+        option = int(input())
+        if option == 1:
+            print(os.system("ifconfig enp0s3 | grep inet* | head -n +1 | cut -c 14-25"))
+        elif option == 2:
+            print(os.system("ifconfig eth0 | grep inet* | head -n +1 | cut -c 14-25"))
+        else:
+            print("Incorrect Option, Exiting!!!")
+    elif ch == 9:
+        path = input("Enter the path of the Directory!\n")
+        print(os.system("ls -lah {}".format(path)))
+    elif ch == 10:
+        path = input("Enter the path of the file!\n")
+        print(os.system("cat {}".format(path)))
+    elif ch == 11:
+        url = input("Enter the URL of the website!\n")
+        print(os.system("ping {}".format(url)))
+    elif ch == 12:
+        username = input("Enter the name of the user to add!\n")
+        os.system("useradd {}".format(username))
+    elif ch == 13:
+        username = input("Enter the name of the user to delete!\n")
+        os.system("userdel {}".format(username))
+    elif ch == 14:
+        os.system("df -h")
+    elif ch == 15:
+        os.system("lsblk")
 
 
 while True:
-    print("""//////////////////////////////////////////////Important Note: This program is best supported on OS based on CentOS//////////////////////////////////////////////\n
-     Press 1: To Run Command Locally!\nPress 2: To run Command Remotely!\n Press 3 To exit from the Program!\n""")
+    print("""//////////////////////////////////////////////Important Note: This program is best supported on OS based 
+    on CentOS//////////////////////////////////////////////\n Press 1: To Run Command Locally!\nPress 2: To run 
+    Command Remotely!\n Press 3 To exit from the Program!\n""")
     choice = int(input())
 
     if choice == 1:
-        print("Press 1 to run any command!\nPress 2 to use the functionalities supported by this software!\n")
+        print("Press 1 to run any command!\nPress 2 to use the functionality supported by this software!\n")
         ch = int(input())
         if ch == 1:
             try:
@@ -74,99 +103,3 @@ while True:
     else:
         print("Wrong Option!")
 
-    '''def linxcmnd():
-        if int(channel) == 1:
-            os.system("clear")
-            print("			Linux Commands			")
-            print("""\n
-			Press 1: To run Date
-			Press 2: To run Calender
-			Press 4: To know the host/network
-			Press 5: To create new blank file in linux 
-			Press 6: To check connectivity status 
-			Press 7: To Add a new user
-			Press 8: To remove a user
-			Press 9: To make a new Directory
-			Press 0: To Exit
-
-			""")
-
-            x = input("Enter your Choice: ")
-            print(x)
-
-            if int(x) == 1:
-                os.system("date")
-
-            elif int(x) == 2:
-                os.system("cal")
-
-            elif int(x) == 3:
-                os.system("ifconfig")
-
-            elif int(x) == 4:
-                os.system("hostname -I")
-
-            elif int(x) == 5:
-                File_name = input("Enter the filename you want to create: ")
-                os.system("touch {0}".format(File_name))
-
-            elif int(x) == 6:
-                os.system("ping")
-
-            elif int(x) == 7:
-                user_name = input("Enter the User name you want to Add: ")
-                os.system("useradd {0}".format(user_name))
-                os.system("getent passwd | grep {0}".format(user_name))
-                print("User Added")
-
-            elif int(x) == 8:
-                user_name = input("Enter the User namee you want to Delete: ")
-                os.system("userdel {0}".format(user_name))
-                print("User Deleted")
-
-            elif int(x) == 9:
-                dir_name = input("Enter the Directory name you want to create: ")
-                os.system("mkdir {0}".format(dir_name))
-
-            elif int(x) == 0:
-                os.system("exit")
-
-        linxcmnd()'''
-
-
-    def docker():
-        if int(channel) == 2:
-            os.system("clear")
-            print("			Let us Configure Docker 			")
-            print("""\n
-			For Configuring DOCKER press the following option stepwise...
-			Press 1: Configure yum for Docker 
-			Press 2: To install Docker 
-			Press 3: To start Docker services 
-			Press 4: To check docker status if running
-			Press 0: To Exit
-			""")
-
-            x = input("Enter your Choice: ")
-            print(x)
-            if int(x) == 1:
-                dock_repo = input("Enter docker repository : ")
-                os.system("cp {0} /etc/yum.repos.d".format(dock_repo))
-                os.system("yum repolist")
-                print("Repolist Checked ")
-                os.system("yum list docker -ce")
-
-            elif int(x) == 2:
-                os.system("yum install docker -ce --nobest")
-
-            elif int(x) == 3:
-                os.system("rpm -q docker -ce")
-                os.system("systemctl start docker")
-                print("Docker service started..")
-
-            elif int(x) == 4:
-                os.system("systemctl status docker")
-                print("Now you install container-O.S .")
-        docker()
-
-input("\nPlease Enter to Contiune..")
